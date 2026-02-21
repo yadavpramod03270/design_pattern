b72f8aaa1fee1e8e9188b1f18d32846d655ae5c0
Pramod Kumar Yadav <168231756+yadavpramod03270@users.noreply.github.com>
2026-02-21 23:21:47 +0530

    Implement ATM proxy demo in maindemo.cpp

--- Proxy_design_pattern/maindemo.cpp
+++ Proxy_design_pattern/maindemo.cpp
@@ -1,1 +1,22 @@
+#include "Bank_Account.h"
+#include "AtmProxy.h"
+
+int main() {
+
+    Atm_machine* bank = new Bank_Account();
+    Atm_machine* atm = new AtmProxy(bank);
+
+    if (atm->login()) {
+        atm->fetch_balance();
+        atm->Withdraw(300);
+        atm->Deposit(20000);
+        atm->fetch_balance();
+        atm->logout();
+    }
+
+    delete atm;
+    delete bank;
+
+    return 0;
+}
 
