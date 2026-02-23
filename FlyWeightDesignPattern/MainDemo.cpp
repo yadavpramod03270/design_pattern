4b57c89a6c1cc10c0c3960a9e2e849376dc95a8a
Pramod Kumar Yadav <168231756+yadavpramod03270@users.noreply.github.com>
2026-02-23 21:47:55 +0530

    Implement main demo for Flyweight pattern
    Demonstrate usage of ChairFactory and display method.

--- FlyWeightDesignPattern/MainDemo.cpp
+++ FlyWeightDesignPattern/MainDemo.cpp
@@ -1,1 +1,20 @@
+#include "ChairFactory.h"
+
+int main() {
+
+    ChairFactory factory;
+
+    ChairType* chair =
+        factory.getChair("Modern Classroom Chair",
+            "Brown",
+            "Wood");
+
+    // Extrinsic state changes
+    chair->display(1, "Amit");
+    chair->display(2, "Ravi");
+    chair->display(3, "Pramod");
+    chair->display(4, "David");
+
+    return 0;
+}
 
