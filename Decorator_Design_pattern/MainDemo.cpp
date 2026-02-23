9fae553726140ef0ae90ff687abd648f66acb0b4
Pramod Kumar Yadav <168231756+yadavpramod03270@users.noreply.github.com>
2026-02-23 11:48:14 +0530

    Enhance coffee with milk and sugar decorators
    Added functionality to decorate coffee with milk and sugar.

--- Decorator_Design_pattern/MainDemo.cpp
+++ Decorator_Design_pattern/MainDemo.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include "simple_coffee.h"
+#include "addmilk.h"
+#include "addsuger.h"
+//#include "addcream.h"
+
+using namespace std;
+
+int main() {
+
+    coffee* myCoffee = new simple_coffee();
+    myCoffee = new addmilk(myCoffee);
+    myCoffee = new addsuger(myCoffee);
+    // myCoffee = new addcream(myCoffee);
+
+    cout << myCoffee->description() << endl;
+    cout << "Total Cost: " << myCoffee->cost() << endl;
+
+    delete myCoffee;
+    return 0;
+}
 
