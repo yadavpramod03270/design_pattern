996f8727cb12fe71b57aa62226b4569270a6309a
Pramod Kumar Yadav <168231756+yadavpramod03270@users.noreply.github.com>
2026-02-23 17:29:07 +0530

    Implement main demo for facade design pattern

--- FacadeDesignPattern/MainDemo.cpp
+++ FacadeDesignPattern/MainDemo.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+#include "HotelManagement.h"
+#include "SelectCuisine.h"
+
+using namespace std;
+
+int main() {
+
+    HotelManagement hotel;
+
+    hotel.reception();
+
+    int choice;
+    cout << "\nSelect Cuisine:\n";
+    cout << "1. Thai\n";
+    cout << "2. Italian\n";
+    cout << "3. Japanese\n";
+    cout << "Enter your choice: ";
+    cin >> choice;
+
+    switch (choice) {
+    case THAI:
+        hotel.thai_menu();
+        break;
+    case ITALIAN:
+        hotel.italian_menu();
+        break;
+    case JAPANESE:
+        hotel.japanese_menu();
+        break;
+    default:
+        cout << "Invalid choice!\n";
+    }
+
+    return 0;
+}
 
