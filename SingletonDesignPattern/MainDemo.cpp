208ddbe004392c7ccb7423c6b65539ae893bb89d
Pramod Kumar Yadav <168231756+yadavpramod03270@users.noreply.github.com>
2026-02-25 14:08:40 +0530

    Implement  Singleton Design Pattern

--- SingletonDesignPattern/MainDemo.cpp
+++ SingletonDesignPattern/MainDemo.cpp
@@ -1,1 +1,15 @@
+#include "PrintingPress.h"
 
+int main() {
+
+    PrintingPress::getInstance()
+        .printNews("Reporter A", "Election results announced");
+
+    PrintingPress::getInstance()
+        .printNews("Reporter B", "Stock market hits new high");
+
+    PrintingPress::getInstance()
+        .printNews("Reporter C", "Weather alert issued");
+
+    return 0;
+}
