de1f919c9453f00ac2bcd27a6117f9eb66c19188
Pramod Kumar Yadav <168231756+yadavpramod03270@users.noreply.github.com>
2026-02-25 20:16:59 +0530

    Implement Chain of Responsibility design pattern

--- ChainOfResponsibilityDesignPattern/MainDemo.cpp
+++ ChainOfResponsibilityDesignPattern/MainDemo.cpp
@@ -1,1 +1,21 @@
+#include "OfficeManager.h"
+#include "OfficeHR.h"
+#include "OfficeDirector.h"
+
+int main() {
+
+    OfficeDirector director;
+    OfficeHR hr;
+    OfficeManager manager;
+
+    // Chain: Manager → HR → Director
+    manager.setNext(&hr);
+    hr.setNext(&director);
+
+    manager.handleRequest(1);   // Manager
+    manager.handleRequest(4);   // HR
+    manager.handleRequest(10);  // Director
+
+    return 0;
+}
 
