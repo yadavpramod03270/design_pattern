98d66bdd465fa3a231ba107f3df533690a6daabb
Pramod Kumar Yadav <168231756+yadavpramod03270@users.noreply.github.com>
2026-02-25 20:37:49 +0530

    Implement  Iterator Design Pattern
    Demonstrate the use of the Playlist and Iterator classes.

--- IteratorDesignPattern/MainDemo.cpp
+++ IteratorDesignPattern/MainDemo.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <vector>
+#include "Playlist.h"
+
+using namespace std;
+
+int main() {
+
+    vector<string> songs = {
+        "Shape of You",
+        "Blinding Lights",
+        "Believer",
+        "Perfect"
+    };
+
+    Playlist myPlaylist(songs);
+
+    Iterator* it = myPlaylist.createIterator();
+
+    cout << "Playing Playlist:\n";
+
+    while (it->hasNext()) {
+        cout << it->next() << endl;
+    }
+
+    delete it;  // clean up
+
+    return 0;
+}
 
