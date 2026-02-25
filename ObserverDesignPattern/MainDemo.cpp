7c48cb78a1f38e6bfd343b7402ec6b9026b207c1
Pramod Kumar Yadav <168231756+yadavpramod03270@users.noreply.github.com>
2026-02-25 20:28:20 +0530

    Implement  Observer Design Pattern

--- ObserverDesignPattern/MainDemo.cpp
+++ ObserverDesignPattern/MainDemo.cpp
@@ -1,1 +1,26 @@
 
+#include "YoutubeChannel.h"
+#include "User.h"
+
+int main() {
+
+    YoutubeChannel techChannel;
+
+    User u1("Pramod");
+    User u2("Pradeep");
+    User u3("David");
+    User u4("Caprio");
+
+    techChannel.subscribe(&u1);
+    techChannel.subscribe(&u2);
+    techChannel.subscribe(&u4);
+
+    techChannel.uploadVideo("Observer Pattern Explained");
+
+    techChannel.subscribe(&u3);
+    techChannel.uploadVideo("Factory Pattern Explained");
+
+    techChannel.unsubscribe(&u4);
+
+    return 0;
+}
