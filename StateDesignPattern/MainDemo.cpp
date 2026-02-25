b774d318e3e38648da1b6e73be5a84a9767a5437
Pramod Kumar Yadav <168231756+yadavpramod03270@users.noreply.github.com>
2026-02-25 20:59:36 +0530

    Implement  state design Pattern

--- StateDesignPattern/MainDemo.cpp
+++ StateDesignPattern/MainDemo.cpp
@@ -1,1 +1,14 @@
+#include "TrafficLight.h"
+#include "RedState.h"
 
+int main() {
+
+    TrafficLight light(new RedState());
+
+    light.requestChange();
+    light.requestChange();
+    light.requestChange();
+    light.requestChange();
+
+    return 0;
+}
