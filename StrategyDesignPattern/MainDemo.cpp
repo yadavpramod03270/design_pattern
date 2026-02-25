f2c0eeadd92e84a5d5e0e8cf3fb32b6760172c20
Pramod Kumar Yadav <168231756+yadavpramod03270@users.noreply.github.com>
2026-02-25 21:08:52 +0530

    Implemented Strategy Design Pattern

--- StrategyDesignPattern/MainDemo.cpp
+++ StrategyDesignPattern/MainDemo.cpp
@@ -1,1 +1,22 @@
+#include "Bicycle.h"
+#include "Bike.h"
+#include "Car.h"
+#include "ChooseStrategy.h"
 
+int main() {
+
+    Bicycle bicycle;
+    Bike bike;
+    Car car;
+
+    ChooseStrategy strategy(&bicycle);
+    strategy.startJourney(200);
+
+    strategy.setStrategy(&bike);
+    strategy.startJourney(500);
+
+    strategy.setStrategy(&car);
+    strategy.startJourney(1000);
+
+    return 0;
+}
