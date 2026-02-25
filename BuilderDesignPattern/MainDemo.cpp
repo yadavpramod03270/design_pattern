faa63c86444cbf6b385f4f5f9182bcc7474bea49
Pramod Kumar Yadav <168231756+yadavpramod03270@users.noreply.github.com>
2026-02-25 13:58:57 +0530

    Implement Builder design pattern

--- BuilderDesignPattern/MainDemo.cpp
+++ BuilderDesignPattern/MainDemo.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include "Director.h"
+#include "WoodenHouseBuilder.h"
 
+using namespace std;
+
+int main() {
+
+    Director director;
+    HouseBuilder* builder = new WoodenHouseBuilder();
+
+    House* house = director.createHouse(builder);
+    house->show();
+
+    delete house;
+    delete builder;
+
+    return 0;
+}
