3938ce176ae25df4929ccabb9db373c09df4ae27
Pramod Kumar Yadav <168231756+yadavpramod03270@users.noreply.github.com>
2026-02-25 20:50:19 +0530

    Implemented Command Design Pattern

--- CommandDesignPattern/MainDemo.cpp
+++ CommandDesignPattern/MainDemo.cpp
@@ -1,1 +1,22 @@
+#include "Chef.h"
+#include "BurgerOrder.h"
+#include "PizzaOrder.h"
+#include "Waiter.h"
 
+int main() {
+
+    Chef chef;
+
+    BurgerOrder burger(&chef);
+    PizzaOrder pizza(&chef);
+
+    Waiter waiter;
+
+    waiter.takeOrder(&burger);
+    waiter.placeOrder();
+
+    waiter.takeOrder(&pizza);
+    waiter.placeOrder();
+
+    return 0;
+}
