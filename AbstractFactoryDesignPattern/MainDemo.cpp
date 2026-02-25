0528bf0fbb37512da4532703bd0cd1515469714c
Pramod Kumar Yadav <168231756+yadavpramod03270@users.noreply.github.com>
2026-02-25 09:58:59 +0530

    Implement main demo for ModernFurnitureFactory

--- AbstractFactoryDesignPattern/MainDemo.cpp
+++ AbstractFactoryDesignPattern/MainDemo.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include "ModernFurnitureFactory.h"
+// #include "VictorianFurnitureFactory.h"
+
+using namespace std;
+
+int main() {
+
+    FurnitureFactory* factory = new ModernFurnitureFactory();
+    // FurnitureFactory* factory = new VictorianFurnitureFactory();
+
+    Chair* chair = factory->createChair();
+    Sofa* sofa = factory->createSofa();
+
+    chair->sitOn();
+    sofa->lieOn();
+
+    delete chair;
+    delete sofa;
+    delete factory;
+
+    return 0;
+}
 
